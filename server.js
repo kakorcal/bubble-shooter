2aea0c7b2558b277f44e243dba062a5b182e180c
Kenneth Korcal <kakorcal@gmail.com>
2018-04-25 11:28:52 -0700

    adding morgan

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const morgan = require('morgan');
 const ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
 // https://expressjs.com/en/advanced/best-practice-performance.html
 app.use(require('helmet')());
 app.use('/static', express.static(path.join(__dirname, '/static')));
 
 if(ENV === 'development') {
+    console.log('ENV', ENV);
     const webpack = require('webpack');
     const webpackDevMiddleware = require('webpack-dev-middleware');
     const config = require('./webpack.config.js');
@@ -27,6 +30,7 @@ if(ENV === 'development') {
         heartbeat: 10 * 1000
     }));
 }else {
+    console.log('ENV', ENV);
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'public/dist/index.html'));
     });
