cba7f491f6b2be9dcd2d8bbf7fb0b37390e9beef
Kenneth Korcal <kakorcal@gmail.com>
2018-04-25 15:43:16 -0700

    update webpack prod

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -19,7 +19,7 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'static/dist'),
-        publicPath: '/dist'
+        publicPath: '/static/dist'
     },
     module: {
         rules: [
